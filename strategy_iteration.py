1beea319a9790dabcd454797847f266775494029
Henry Xie <henryxie@college.harvard.edu>
2023-12-03 20:13:10 -0500

    henry pdate

--- strategy_iteration.py
+++ strategy_iteration.py
@@ -1,71 +1,67 @@
 import numpy as np
 import mdp   
+import unittest
 
 class DP:
     def __init__(self, MDP):
         self.MDP = MDP
-        self.invalid_states = None
 
-    # Creates a vector of state indices where you must take and cannot pass
-    def init_action_mask():
-        num_states = mdp.calc_S(self.N, self.K)
-        temp = np.arange(num_states)
-    
-        def is_valid(index):
-            return self.idx2state[index] != 0
+    def oneStep(self, prev_V):
+        value = self.MDP.R + np.sum(self.MDP.P * prev_V.reshape(1, 1, -1), axis=2)
+        policy = np.argmax(value, axis = 0)
+        new_V = np.amax(value, axis = 0)
 
-        self.invalid_states = is_valid(temp).astype(int)
+        return new_V, policy
 
-    def oneStep(prev_V):
-        value = self.R + np.sum(self.P * prev_V.reshape(1, 1, -1), axis=2)
-        policy = np.argmax(value, axis = 0) * self.invalid_states
-        new_V = value[policy, np.arange(value.shape[1])]
-
-        return new_V, policy * self.invalid_states
-
-    def fullDP(horizon):
-        prev_V = np.zeros(mdp.calc_S(self.MDP.N, self.MDP.K))
+    def fullDP(self, horizon):
+        prev_V = np.zeros(self.MDP.num_states)
         policy_h = None
 
         for _ in range(horizon):
-            prev_V, policy_h = oneStep(prev_V)
+            prev_V, policy_h = self.oneStep(prev_V)
 
         return policy_h
 
 class StrategyIteration:
-    def __init__(self, N, K, H, optimization_method, num_iterations):
+    def __init__(self, N, K, optimization_method, num_iterations):
         self.N = N
         self.K = K
-        self.H = H
         self.num_iterations = num_iterations
-        self.policy_history = []
+        self.prev_policy = None
         self.optimization_method = optimization_method
 
-        init_start_policy()
+        state2idx, _ = mdp.get_mappings(self.N, self.K)
+        self.num_states = len(state2idx)
 
-    def reset():
-        self.policy_history = []
+        self.prev_policy = np.random.choice([0, 1], size=self.num_states)
 
-    def init_start_policy():
-        num_states = mdp.calc_S(self.N, self.K)
-        self.policy_history.append(np.random.choice([0, 1], size=num_states))
-
-    def get_p2_policy():
-        return self.policy_history[-1] # Could replace with something more fancy later
-    
-    def iteration_step():
-        prev_policy = get_p2_policy()
-        MDP = build_nothanks_mdp(N, K, prev_policy)
+    def iteration_step(self):
+        MDP = mdp.build_nothanks_mdp(self.N, self.K, self.prev_policy)
         new_policy = None
 
         if self.optimization_method == 'DP':
             dp = DP(MDP)
-            new_policy = dp.fullDP(self.H)
+            new_policy = dp.fullDP(MDP.H)
         
-        self.policy_history.append(new_policy)
+        self.prev_policy = new_policy
 
-    def full_iteration():
+    def full_iteration(self):
         for _ in range(self.num_iterations):
-            iteration_step()
+            self.iteration_step()
 
-        return self.policy_history[-1]
+        return self.prev_policy
+
+class TestStrategyIteration(unittest.TestCase):
+    def test_strategy_iteration(self):
+        """
+        Test that strategy iteration works.
+        """
+        N = 5
+        K = 2
+        num_iterations = 1
+        si = StrategyIteration(N, K, 'DP', num_iterations)
+        pi_1 = si.full_iteration()
+        evaluate_policy(N, K, pi_1)
+
+if __name__ == '__main__':
+    unittest.main()
